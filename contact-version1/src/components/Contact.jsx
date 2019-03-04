c3b43bc54e65454ac2b37815d90972b0e8e63424
Mohammad Seyedsaidi <mohammad.seyedsaidi@auto1.com>
2019-03-04 15:59:11 +0100

    context or not context thats the matter

--- contact-version1/src/components/Contact.jsx
+++ contact-version1/src/components/Contact.jsx
@@ -4,28 +4,50 @@ import PropTypes from "prop-types";
 class Contact extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      showContactInfo: false
+    };
   }
-  onShowClick = e => {};
+  onShowClick = () => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
   render() {
     const { contact } = this.props;
     return (
       <div className="card card-body mb-3">
         <h4>
           {contact.name}{" "}
-          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          <i
+            style={{ cursor: "pointer" }}
+            onClick={this.onShowClick}
+            className="fas fa-sort-down"
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {contact.email}</li>
-          <li className="list-group-item">Phone : {contact.phone}</li>
-        </ul>
+        {this.state.showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {contact.email}</li>
+            <li className="list-group-item">Phone : {contact.phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- contact-version1/src/components/Contacts.jsx
+++ contact-version1/src/components/Contacts.jsx
@@ -1,40 +1,38 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "./../context";
 
 class Contacts extends Component {
   constructor() {
     super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: " John Doe",
-          email: "jdoe@gmail.com",
-          phone: "5555-55-5555"
-        },
-        {
-          id: 2,
-          name: "Karen wiliams",
-          email: "karen@gmail.com",
-          phone: "222-222-2222"
-        },
-        {
-          id: 3,
-          name: "henry johnson",
-          email: "henry@gmail.com",
-          phone: "1111-111-1111"
-        }
-      ]
-    };
   }
+
+  deleteContact = id => {
+    const newContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
+    this.setState({
+      contacts: newContacts
+    });
+  };
   render() {
-    const { contacts } = this.state;
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
